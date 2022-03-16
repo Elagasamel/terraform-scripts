cf040083bbe77ad05d6bd455ee4d8df7987898f4
Elagas Amel <63187083+Elagasamel@users.noreply.github.com>
2022-03-16 12:54:47 +0100

    Create variables.tf

---
+++ components/api_gateway/variables.tf
@@ -0,0 +1,66 @@
+# Required variables:
+variable "name" {
+  description = "The application name."
+}
+
+variable "environment" {
+  description = "The environment for this gateway: development, qa, or production."
+}
+
+variable "stack" {
+  description = "The 'stack' for this gateway: web, sms, backend, data."
+}
+
+variable "functions_count" {
+  # Temporary hack to work around Terraform 0.11 limitation. <https://git.io/fjLYC>
+}
+
+variable "functions" {
+  description = "The functions that this gateway is allowed to invoke."
+  type        = list(string)
+}
+
+# The root route:
+variable "root_function" {
+  description = "The root route's function handler."
+}
+
+variable "root_method" {
+  description = "The root route's HTTP method."
+  default     = "ANY"
+}
+
+variable "root_authorization" {
+  description = "The root route's authorization type."
+  default     = "NONE"
+}
+
+variable "root_authorizer_id" {
+  description = "If 'var.root_authorization' is 'CUSTOM', the Lambda to authorize with."
+  default     = ""
+}
+
+# Additional routes:
+variable "routes" {
+  description = "A list of routes this gateway can respond to."
+
+  default = []
+}
+
+variable "routes_count" {
+  # Temporary hack to work around Terraform 0.11 limitation. <https://git.io/fjLYC>
+}
+
+# Optional custom domain support:
+variable "domain" {
+  description = "The domain this application will be accessible at, e.g. lambda.dosomething.org"
+
+  # If omitted, we will just not attach a custom domain to this app. By default,
+  # you can access a Lambda function at a URL returned in the `base_url` output.
+  default = ""
+}
+
+variable "certificate" {
+  description = "The ACM certificate to use for this domain, e.g. *.dosomething.org"
+  default     = "*.dosomething.org"
+}
