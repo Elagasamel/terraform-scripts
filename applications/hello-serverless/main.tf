39e96d07a089ca00cb5816e3824911e40a5c133c
Elagas Amel <63187083+Elagasamel@users.noreply.github.com>
2022-03-16 12:53:58 +0100

    Create main.tf

---
+++ applications/hello-serverless/main.tf
@@ -0,0 +1,28 @@
+variable "logger" {
+  description = "The Lambda function ARN to subscribe to this function's log group."
+}
+
+module "app" {
+  source = "../../components/lambda_function"
+
+  application = "hello-serverless"
+  name        = "hello-serverless"
+  environment = "development"
+  stack       = "web"
+
+  runtime = "nodejs12.x"
+  logger  = var.logger
+}
+
+module "gateway" {
+  source = "../../components/api_gateway_proxy"
+
+  name        = "hello-serverless"
+  environment = "development"
+  stack       = "web"
+
+  function_arn        = module.app.arn
+  function_invoke_arn = module.app.invoke_arn
+  domain              = "hello-serverless.dosomething.org"
+}
+
